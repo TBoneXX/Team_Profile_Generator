3dd6905d6ec5dab100f917569cdfb1a327c43c63
Mark Turner <mark.edward.turner@gmail.com>
2022-11-22 13:21:51 -0500

    updated engineer test code

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,15 +1,22 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-  //code goes here
+  const github = "github";
+  const e = new Engineer("Eric",23,"eric@test.com",github);
+  expect(e.github).toBe(github);
+
 });
 
 test("getRole() should return \"Engineer\"", () => {
-  //code goes here
+   const testValue = "Engineer";
+  const e = new Engineer("Eric", 23,"eric@test.com", "github");
+  expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
-  //code goes here
+  const testValue = "github";
+  const e = new Engineer("Eric",23,"eric@test.com",testValue);
+  expect(e.getGithub()).toBe(testValue);
 });
 
 
